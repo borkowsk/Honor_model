b582e340c6c2aee78de13374fbcd58e3c0165079
Wojciech T. Borkowski <wborkowsk@gmail.com>
2022-07-05 12:47:31 +0200

    Unix_cleaning_tool_added

---
+++ _cleanProject.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#@author Wojciech Borkowski ()
+
+if [ $# -ne 0 ]; 
+then
+   echo "No parameters expected!" 1>&2
+   exit -1
+fi
+
+echo "Cleaning cmake files!"
+echo "Cleaning cmake files" `date` >> .clean_list.txt
+
+for f in *; do # * rozwija się do listy wszystkich plików/katalogów znajdujących się w bieżącym katalogu
+  if [ -d "$f" ]; then
+    pushd "$f"
+
+    rm -f CMakeCache.txt
+    rm -f Makefile
+    rm -f cmake_install.cmake
+    rm -Rf CMakeFiles
+    rm -Rf cmake-build-*/
+
+    popd > /dev/null
+  elif [ -f "$f" ]; then
+    echo "'$f' plik" >> /dev/null
+  else
+    echo "'$f' unexpected directory entry type" >> unexpected.txt
+  fi
+done
+
+rm -f CMakeCache.txt
+rm -f Makefile
+rm -f cmake_install.cmake
+rm -Rf CMakeFiles
+rm -Rf cmake-build-*/
+
+#rm -f build.ninja
+#rm -f rules.ninja
+#rm -f .ninja_log
+#rm -f .ninja_deps
